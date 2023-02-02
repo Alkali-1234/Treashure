24a0e01a8eb3e54b598fd08b961000395cb95976
Alkali1234 <merril.faizun@gmail.com>
2023-02-02 18:24:33 +0700

    Added Login and Signup page, missing: Login/Signup button functionality, input functionality, background image

--- screens/Login_Signup.js
+++ screens/Login_Signup.js
@@ -1,27 +1,85 @@
-import React from 'react'
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, { useEffect, useState } from 'react'
+import {View, Text, StyleSheet, TouchableOpacity, TextInput} from 'react-native';
 
 function Login_Signup( {navigation} ) {
     
-    const navigateToHome = ()=>{
-        navigation.navigate("Home");
-    }
+    const [signUp, toggleSignUp] = useState(false);
 
-  return (
-    <View>
-        <Text>Login/SignupPage (real)</Text>
-        <TouchableOpacity onPress={navigateToHome}>
-            <Text>Navigate to Home</Text>
-        </TouchableOpacity>
-    </View>
-  )
+    useEffect(() => {
+        navigation.setOptions({headerShown: false})
+    })
+
+    return (
+        signUp? (
+            <View style={styles.container}>
+                <View style={styles.mainTitleContainer}>
+                    <Text style={{fontSize: 30, marginBottom: 5}}>Treashures</Text>
+                    <Text style={{color: '#756666', fontStyle: 'italic'}}>"Heaps of treasures lie among'st our trash"</Text>
+                </View>
+                <View style={{marginTop: 75}}>
+                    <TextInput style={styles.input} placeholder="Username" />
+                    <TextInput style={styles.input} placeholder="Email" />
+                    <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} />
+                    <TextInput style={styles.input} placeholder="Retype Password" secureTextEntry={true} />
+                    <View style={{flexDirection: 'row'}}>
+                        <Text style={{color: '#756666'}}>Already have an account? </Text>
+                        <TouchableOpacity onPress={() => toggleSignUp(!signUp)}><Text style={{color: 'blue'}}>Log In</Text></TouchableOpacity> 
+                    </View>              
+                </View>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={{color:'white'}}>Sign Up</Text>
+                </TouchableOpacity>
+            </View> 
+        ):(
+            <View style={styles.container}>
+                <View style={styles.mainTitleContainer}>
+                    <Text style={{fontSize: 30, marginBottom: 5}}>Treashures</Text>
+                    <Text style={{color: '#756666', fontStyle: 'italic'}}>"Heaps of treasures lie among'st our trash"</Text>
+                </View>
+                <View style={{marginTop: 75}}>
+                    <TextInput style={styles.input} placeholder="Email/Username" />
+                    <TextInput style={[styles.input, {marginTop:62}]} placeholder="Password" secureTextEntry={true} />
+                    <View style={{flexDirection: 'row'}}>
+                        <Text style={{color: '#756666'}}>Dont have an account? </Text>
+                        <TouchableOpacity onPress={() => toggleSignUp(!signUp)}><Text style={{color: 'blue'}}>Sign Up!</Text></TouchableOpacity> 
+                    </View>
+                                  
+                </View>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={{color:'white'}}>Login</Text>
+                </TouchableOpacity>
+            </View>  
+        )
+
+      
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'lightblue'
     },
+    mainTitleContainer: {
+        alignItems: 'center',
+        marginTop: 150
+    },
+    input: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        padding: 1,
+        width: 265
+    },
+    button: {
+        height: 40,
+        width: 265,
+        backgroundColor: "#003EDD",
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        marginTop: 60
+    }
 })
 
 export default Login_Signup;
